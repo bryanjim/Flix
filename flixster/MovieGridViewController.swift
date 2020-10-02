aa4cf1a29479854cbffc20ba1e186c0ffb3c9794
Bryan Jimenez <bryan@roguedevs.io>
2020-10-01 21:21:33 -0700

    Setup collection view

--- flixster/MovieGridViewController.swift
+++ flixster/MovieGridViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class MovieGridViewController: UIViewController {
+class MovieGridViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieGridCell", for: indexPath) as! MovieGridCell
+        let movie = movies[indexPath.item]
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)!
+        
+        cell.posterView.af_setImage(withURL: posterUrl)
+        
+        return cell
+    }
+    
     var movies = [[String:Any]]()
     
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -24,10 +46,11 @@ class MovieGridViewController: UIViewController {
         let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 
         self.movies = dataDictionary["results"] as! [[String:Any]]
-        print(self.movies)
+        self.collectionView.reloadData()
         }
+    }
         //self.tableView.reloadData()
-
+            task.resume()
         // Do any additional setup after loading the view.
     }
     
